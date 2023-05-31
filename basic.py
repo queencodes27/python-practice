d20398118ae3836289626ea28ab3fa88a15fdede
nsaip <nadiasaip@gmail.com>
2023-05-31 10:37:20 -0500

    sum solved

---
+++ basic.py
@@ -0,0 +1,52 @@
+"""
+Exercise 1: Calculate the multiplication and sum of two numbers
+Given two integer numbers return their product only if the product is equal to or lower than 1000, 
+else return their sum.
+
+"""
+# number1 = 20
+# number2 = 30
+# if number1 * number2 <= 1000:
+#     print (number1 * number2)
+# else:
+#     print(number2 + number1)
+
+# number1 = 40
+# number2 = 30
+# if number1 * number2 <= 1000:
+#     print (number1 * number2)
+# else:
+#     print(number2 + number1)
+
+# # FUNCTION
+# def product_or_sum(num1, num2):
+#     product = num1 * num2
+#     if product <= 1000:
+#         return product
+#     else:
+#         return num1 + num2
+# print(product_or_sum(20, 30))
+# print(product_or_sum(40,30))
+
+
+
+"""
+Exercise 2: Print the sum of the current number and the previous number
+Write a program to iterate the first 10 numbers and in each iteration, print the sum of the 
+current and previous number.
+
+"""
+n = 0
+for i in range(10):
+    sum = 0 + i
+    
+
+
+
+
+
+
+
+
+
+
