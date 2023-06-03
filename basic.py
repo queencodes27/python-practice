8e16373a1054d61e898dd1d4212700e7855f5969
nsaip <nadiasaip@gmail.com>
2023-06-03 06:33:16 -0500

    changes made

--- basic.py
+++ basic.py
@@ -66,4 +66,6 @@ for i in range(0, len(str)):
         list.append(a)
     else:
         pass
+
+    
         
--- dict.py
+++ dict.py
@@ -78,6 +78,48 @@ Exercise 6: Delete a list of keys from a dictionary
 
 
 """
-Exercise 7: Check if a value exists in a dictionary
+Exercise 7: Check if 200 value exists in a dictionary
 """
-sample_dict = {'a': 100, 'b': 200, 'c': 300}
+# sample_dict = {'a': 100, 'b': 200, 'c': 300}
+# values = sample_dict.values()
+# if 200 in sample_dict.values():
+#     print("yes 200 exists")
+# else:
+#     print("no it doesn't")
+"""
+Exercise 8: Rename key of a dictionary
+Write a program to rename a key city to a 
+location in the following dictionary.
+
+"""
+# sample_dict = {
+#   "name": "Kelly",
+#   "age":25,
+#   "salary": 8000,
+#   "city": "New york"
+# }
+# sample_dict['location'] = sample_dict.pop('city')
+# print(sample_dict)
+"""
+Exercise 9:
+Get the key of a minimum value 
+from the following dictionary
+"""
+# sample_dict = {
+#   'Physics': 82,
+#   'Math': 65,
+#   'history': 75
+# }
+# print(min(sample_dict))
+"""
+Exercise 10
+Write a Python program to 
+change Brad’s salary to 8500 in the following dictionary
+"""
+# sample_dict = {
+#     'emp1': {'name': 'Jhon', 'salary': 7500},
+#     'emp2': {'name': 'Emma', 'salary': 8000},
+#     'emp3': {'name': 'Brad', 'salary': 500}
+# }
+# sample_dict["emp3"] = {'name': 'Brad', 'salary': 8500}
+# print(sample_dict)
