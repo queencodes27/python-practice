829681749e04fc3fa274c22f8173c2513493a928
nsaip <nadiasaip@gmail.com>
2023-06-03 07:53:02 -0500

    basic edited

--- basic.py
+++ basic.py
@@ -58,14 +58,29 @@ that are present at an even index number
 Exercise 4: Remove first n characters from a string
 
 """
-str = input(" enter word:") 
-list = []
-for i in range(0, len(str)):
-    if i % 2 == 0:
-        a = str[i]
-        list.append(a)
-    else:
-        pass
+# str = input(" enter word:") 
+# list = []
+# for i in range(0, len(str)):
+#     if i % 2 == 0:
+#         a = str[i]
+#         list.append(a)
+#     else:
+#         pass
+"""
+Exercise 5: Check if the first and last number of a list is the same
+Write a function to return 
+True if the first and last number of a given list is same. 
+If numbers are different then return False.
+"""
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
 
-    
-        
+def same(list):
+    print("l: ", list)
+    first_num = list[0]
+    last_num = list[-1]
+    if first_num == last_num:
+        return True
+    else:
+        return False
+print()
