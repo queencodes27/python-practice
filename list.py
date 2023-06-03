feb019f6330288436ae12961245fd0ed4ef86987
nsaip <nadiasaip@gmail.com>
2023-06-03 06:53:20 -0500

    list created

---
+++ list.py
@@ -0,0 +1,15 @@
+"""
+Exercise 1: Reverse a list in Python
+"""
+list1 = [100, 200, 300, 400, 500]
+list1.reverse()
+print(list1)
+
+"""
+Exercise 2: Concatenate two lists index-wise
+Write a program to add two lists index-wise. 
+Create a new list that contains the 0th index item 
+from both the list, then the 1st index item, 
+and so on till the last element. any leftover items will get added at the end of the new list.
+
+"""
